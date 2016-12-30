a06e528f0b3aa7807c7171e7e25787c30684af95
Ricardo Bánffy <rbanffy@gmail.com>
2016-12-30 22:14:29 +0000

    Restructuring and improvements (#2)
    * Move actual functionality into a lib
    
    * Add test
    
    * Add tap as devDependency
    
    * Make tests cover suffix generation
    
    * Added .travis.yml file
    
    * Added environment to .travis.yml file
    
    * Added tap to dependencies, removed env from .travis.yml
    
    * Fix package.json
    
    * misc fixes
    
    * We can run under an older node
    
    * Remove unused line
    
    * Misc fixes
    
    * Randomly add a suffix
    
    * Remove tap from dependencies
    
    * Removing travis (for now)

---
+++ test/name-generation.js
@@ -0,0 +1,11 @@
+const tap = require('tap')
+const nsaname = require('../lib/nsaname.js')
+
+var name = nsaname.getNSAName()
+
+tap.true(nsaname.wordLists.first.some(function (n) { return name.indexOf(n) > -1 }))
+tap.true(nsaname.wordLists.second.some(function (n) { return name.indexOf(n) > -1 }))
+
+name = nsaname.getNSAName(true)
+
+tap.true(nsaname.wordLists.suffixes.some(function (n) { return name.indexOf(n) > -1 }))
