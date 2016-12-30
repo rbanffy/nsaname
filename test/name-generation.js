a4d3f3a6e1fa6c11daedfb92c8643b24c5647364
Ricardo Banffy <rbanffy@gmail.com>
2016-12-30 21:11:21 +0000

    Make tests cover suffix generation

--- test/name-generation.js
+++ test/name-generation.js
@@ -6,3 +6,7 @@ var name = nsaname.getNSAName()
 
 tap.true(nsaname.wordLists.first.some(function (n) { return name.indexOf(n) > -1 }))
 tap.true(nsaname.wordLists.second.some(function (n) { return name.indexOf(n) > -1 }))
+
+name = nsaname.getNSAName(true)
+
+tap.true(nsaname.wordLists.suffixes.some(function (n) { return name.indexOf(n) > -1 }))
