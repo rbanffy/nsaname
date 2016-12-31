8a1c98181563299da32e7341ab52b6de60937d14
Ricardo Bánffy <rbanffy@gmail.com>
2016-12-31 23:28:21 +0000

    Add new swtiches (#3)
    * Add Wikipedia reference
    
    * Add new switches
    
    * Remove troublesome words
    
    * Version bump
    
    * Update README
    
    * Add tests for new switches

--- test/name-generation.js
+++ test/name-generation.js
@@ -3,9 +3,17 @@ const nsaname = require('../lib/nsaname.js')
 
 var name = nsaname.getNSAName()
 
-tap.true(nsaname.wordLists.first.some(function (n) { return name.indexOf(n) > -1 }))
-tap.true(nsaname.wordLists.second.some(function (n) { return name.indexOf(n) > -1 }))
+// Test default behavior.
+tap.true(nsaname.wordLists.first.some(
+  function (n) { return name.indexOf(n) > -1 }))
+tap.true(nsaname.wordLists.second.some(
+  function (n) { return name.indexOf(n) > -1 }))
 
+//  Test with suffixes
 name = nsaname.getNSAName(true)
+tap.true(nsaname.wordLists.suffixes.some(
+  function (n) { return name.indexOf(n) > -1 }))
 
-tap.true(nsaname.wordLists.suffixes.some(function (n) { return name.indexOf(n) > -1 }))
+// Test separator
+name = nsaname.getNSAName(false, '-')
+tap.true(name.indexOf('-') > -1)
