07573761055dd2c4adf34a265a6f40899fe7d517
Ricardo Bánffy <ricardo.banffy@carelon.com>
2024-10-23 18:09:32 +0100

    Update dependency versions

--- test/name-generation.js
+++ test/name-generation.js
@@ -4,16 +4,16 @@ const nsaname = require('../lib/nsaname.js')
 var name = nsaname.getNSAName()
 
 // Test default behavior.
-tap.true(nsaname.wordLists.first.some(
+tap.ok(nsaname.wordLists.first.some(
   function (n) { return name.indexOf(n) > -1 }))
-tap.true(nsaname.wordLists.second.some(
+tap.ok(nsaname.wordLists.second.some(
   function (n) { return name.indexOf(n) > -1 }))
 
 //  Test with suffixes
 name = nsaname.getNSAName(true)
-tap.true(nsaname.wordLists.suffixes.some(
+tap.ok(nsaname.wordLists.suffixes.some(
   function (n) { return name.indexOf(n) > -1 }))
 
 // Test separator
 name = nsaname.getNSAName(false, '-')
-tap.true(name.indexOf('-') > -1)
+tap.ok(name.indexOf('-') > -1)
